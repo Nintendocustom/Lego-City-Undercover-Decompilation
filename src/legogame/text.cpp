bd3bbf1b12dee17b1d591c3862897548039857dd
cr7jrsuii <195530488+cr7jrsuii@users.noreply.github.com>
2026-04-04 02:58:25 +0100

    Update text.cpp

--- src/legogame/text.cpp
+++ src/legogame/text.cpp
@@ -24,3 +24,17 @@ void Text_DecodeButtons(char* input_name, char* output_buffer) {
         NuStrCpy(output_buffer, "r1");
     }
 }
+
+int NuStrCpy(char* dst, const char* src) {
+    char* d = dst;
+    if (src && *src) {
+        const char* s = src + 1;
+        char c = *src;
+        do {
+            *d++ = c;
+            c = *s++;
+        } while (c);
+    }
+    *d = 0;
+    return (int)(d - dst);
+}
