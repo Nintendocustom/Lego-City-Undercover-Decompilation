6aaf82626e2a74838f84a30a088a1030a524eb42
Nintendocustom <nintendocustomyt@gmail.com>
2026-06-28 01:29:07 +0200

    Add `chroma` functions `geti`, `_RLE_WriteNonRep`, `RLE_Decode`

--- src/chroma/chroma_utils.h
+++ src/chroma/chroma_utils.h
@@ -1,11 +1,12 @@
 #pragma once
+#include "cstdint"
 
 void Chroma_GenColorConversionPalettes();
-int geti(void* ptr, int offset);
+uint64_t geti(void* ptr, int numBytes);
 void puti(void* ptr, unsigned long value, int offset);
 unsigned int _RLE_WriteRep(unsigned char* dest, unsigned int* destOffset, unsigned char value,
                            unsigned char count, unsigned int maxDestSize);
-unsigned int _RLE_WriteNonRep(unsigned char* dest, unsigned int* destOffset, unsigned char value,
-                              unsigned char count);
+void _RLE_WriteNonRep(unsigned char* dest, unsigned int* destOffset, unsigned char value,
+                      unsigned char count);
 unsigned int RLE_Encode(unsigned char* dest, unsigned char* src, unsigned int srcSize);
-unsigned int RLE_Decode(unsigned char* dest, unsigned char* src, unsigned int srcSize);
+unsigned int RLE_Decode(unsigned char* src, unsigned char* dst, unsigned int srcSize);
